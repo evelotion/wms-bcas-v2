04faba4edc062cb53b760b385de560f335bb3f79
INDRA DWI ANANDA <50587440+evelotion@users.noreply.github.com>
2026-06-29 11:08:24 +0700

    Deleted prisma.config.ts

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'prisma';
-
-export default defineConfig({
-  datasource: {
-    provider: 'postgresql',
-    url: process.env.DATABASE_URL, // Prisma sekarang ambil URL dari sini
-  },
-});
