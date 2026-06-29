fbbded96158ecfb5c0e3a99252325c5731498395
INDRA DWI ANANDA <50587440+evelotion@users.noreply.github.com>
2026-06-29 16:24:59 +0700

    Updated route.ts

--- src/app/api/master/last-serial/[barangId]/route.ts
+++ src/app/api/master/last-serial/[barangId]/route.ts
@@ -1,7 +1,7 @@
+export const dynamic = 'force-dynamic';
 import prisma from '@/lib/prisma';
 import { NextResponse, type NextRequest } from 'next/server';
 
-export const dynamic = 'force-dynamic';
 
 export async function GET(
   request: NextRequest,
